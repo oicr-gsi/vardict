c98eed2c40cedff0aed97f3e82c621585b62fc0e
Gavin Peng <gpeng@oicr.on.ca>
2025-02-10 15:51:55 -0500

    fix syntax

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 cd $1
 
-for f in $(find . -xtype f -name "*.vcf.gz" | sort -V);do zcat $f | grep -v ^# | md5sum
+for f in $(find . -xtype f -name "*.vcf.gz" | sort -V);do zcat $f | grep -v ^# | md5sum; done
